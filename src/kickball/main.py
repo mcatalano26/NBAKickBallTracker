0220d65b4b0ba3bdf6f71c2d6ac4ba1a3a0eceb4
Matt Catalano <mattcat26@gmail.com>
2025-08-07 21:08:34 -0500

    clean up main function

--- src/kickball/main.py
+++ src/kickball/main.py
@@ -11,12 +11,11 @@ from .kickballs import update_kickball_db
 from .player_info import update_minutes_db
 
 
-def main():
+def get_dates():
     
     # This will be run on the first of every month
     yesterday = datetime.now() - timedelta(days=1)
     last_month_name = yesterday.strftime("%B")
-    season = Season.default
     
     # TODO: Use nba_api to get the current season actual start date
     # Hardcoding 2025-26 season start date for now
@@ -25,6 +24,9 @@ def main():
     month_start_date = date(yesterday.year, yesterday.month, 1)
     month_end_date = date(yesterday.year, yesterday.month, yesterday.day)
     
+    return season_start_date, month_start_date, month_end_date, last_month_name
+
+def get_podiums(season_start_date, month_start_date, month_end_date):
     # relatively quick operation, can be done every time
     replace_season_dbs()
     
@@ -37,13 +39,26 @@ def main():
     season_podium = get_podium(season_start_date, month_end_date)
     month_podium = get_podium(month_start_date, month_end_date)
     
+    return season_podium, month_podium
+
+def send_kickball_email(season_podium, month_podium, last_month_name):
     load_dotenv()
     
     sender_email = "thekickballtracker@gmail.com"
     sender_password = os.getenv("EMAIL_PASSWORD")
     recipient_emails = ["mattcat26@gmail.com"]
     
+    season = Season.default
+    
     send_email(season_podium, month_podium, last_month_name, season, sender_email, sender_password, recipient_emails)
+
+
+def main():
+    season_start_date, month_start_date, month_end_date, last_month_name = get_dates()
+        
+    season_podium, month_podium = get_podiums(season_start_date, month_start_date, month_end_date)
+    
+    send_kickball_email(season_podium, month_podium, last_month_name)
 
 
 if __name__ == "__main__":
