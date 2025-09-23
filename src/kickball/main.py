03e186b0e2b66e49fc7e29b234bf975243c14986
Matt Catalano <mattcat26@gmail.com>
2025-09-22 22:13:07 -0500

    one more test

--- src/kickball/main.py
+++ src/kickball/main.py
@@ -76,10 +76,10 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         test()
         
-    today = date.today()
-    if today < SEASON_START_DATE:
-        print(f"Season has not started yet. Season starts on {SEASON_START_DATE}. Exiting.")
-        exit(0)
+    # today = date.today()
+    # if today < SEASON_START_DATE:
+    #     print(f"Season has not started yet. Season starts on {SEASON_START_DATE}. Exiting.")
+    #     exit(0)
 
     param = sys.argv[1]
 
