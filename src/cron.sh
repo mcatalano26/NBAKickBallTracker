99cb5d195862820712568e6c1cd5afeb02ed8bca
Matt Catalano <mattcat26@gmail.com>
2025-09-22 21:24:51 -0500

    test

---
+++ src/cron.sh
@@ -0,0 +1,14 @@
+cd "$(dirname "$0")"
+
+git checkout main
+git pull
+
+# weekly for weekly job
+# monthly for monthly job
+# test for test job
+uv run -m kickball.main "$1"
+
+cd ..
+git add .
+git commit -m "Weekly update"
+git push
